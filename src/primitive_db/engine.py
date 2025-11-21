823611d594b50f10190556e2925d94c7bc523c38
Artyom Nalyotov <whalewander@yandex.ru>
2025-11-21 17:04:33 +0200

    added welcome function into the project

---
+++ src/primitive_db/engine.py
@@ -0,0 +1,4 @@
+def welcome():
+    print("\n***")
+    print("<command> exit - выйти из программы")
+    print("<command> help - справочная информация")
--- src/primitive_db/main.py
+++ src/primitive_db/main.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
+import prompt
+from . import engine
+
 def main():
     print("DB project is running!")
+    engine.welcome()
+    while True:
+        match prompt.string("Введите строку: "):
+            case 'help':
+                engine.welcome()
+            case 'exit':
+                exit()
 
 
 if __name__ == "__main__":
